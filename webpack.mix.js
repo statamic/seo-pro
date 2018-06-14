904e585ed21cd219886888a5e1fba8cc4cb4f662
Jason Varga <jason@pixelfear.com>
2018-06-14 09:45:54 -0400

    Add laravel mix to copy into statamic site

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+const mix = require('laravel-mix');
+require('dotenv').config();
+
+// In an .env file, define the path to the Statamic site you want this addon to be copied to.
+// eg. STATAMIC_PATH=/users/bob/sites/statamic
+const statamic = process.env.STATAMIC_PATH;
+
+mix.copyDirectory('SeoPro', `${statamic}/site/addons/SeoPro`);
