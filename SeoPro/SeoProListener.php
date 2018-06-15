e57e6fd0db1fa4c97197643ee80cac286289f00b
Jason Varga <jason@pixelfear.com>
2018-06-15 15:33:55 -0400

    Each field gets placeholder text of the global fallback

--- SeoPro/SeoProListener.php
+++ SeoPro/SeoProListener.php
@@ -30,6 +30,11 @@ class SeoProListener extends Listener
 
         $fields = YAML::parse(File::get($this->getDirectory().'/fieldsets/content.yaml'))['fields'];
 
+        $fields['seo']['fields'] = collect($fields['seo']['fields'])->map(function ($field, $key) use ($event) {
+            $field['placeholder'] = $this->getPlaceholder($key, $field, $event->data);
+            return $field;
+        })->all();
+
         $sections['seo'] = [
             'display' => 'SEO',
             'fields' => $fields
@@ -38,5 +43,16 @@ class SeoProListener extends Listener
         $contents = $fieldset->contents();
         $contents['sections'] = $sections;
         $fieldset->contents($contents);
+    }
+
+    protected function getPlaceholder($key, $field, $data)
+    {
+        $vars = (new TagData)
+            ->with($this->getConfig('defaults'))
+            ->with($data->get('seo', []))
+            ->withCurrent($data->toArray())
+            ->get();
+
+        return array_get($vars, $key);
     }
 }
