626f8e22905e6c8cee6840444a519ecc1c38906c
Jason Varga <jason@pixelfear.com>
2018-07-02 17:29:19 -0400

    Sitemap gets cached for an hour by default, with ability to change.

--- SeoPro/Controllers/SitemapController.php
+++ SeoPro/Controllers/SitemapController.php
@@ -2,15 +2,16 @@
 
 namespace Statamic\Addons\SeoPro\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Statamic\Addons\SeoPro\Sitemap\Sitemap;
 
 class SitemapController extends Controller
 {
     public function show()
     {
-        $content = $this->view('sitemap', [
-            'sitemap' => new Sitemap
-        ]);
+        $content = Cache::remember('sitemap', $this->getConfig('sitemap_cache_length'), function () {
+            return $this->view('sitemap', ['sitemap' => new Sitemap])->render();
+        });
 
         return response($content)->header('Content-Type', 'text/xml');
     }
