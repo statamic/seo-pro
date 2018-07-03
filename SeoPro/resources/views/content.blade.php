ee890f4a1465ccb78e9ddd1a191951e0a49391e8
Jason Varga <jason@pixelfear.com>
2018-07-03 15:07:56 -0400

    Whitespace

--- SeoPro/resources/views/content.blade.php
+++ SeoPro/resources/views/content.blade.php
@@ -9,53 +9,55 @@
     <div class="flex items-center mb-1">
         <h2 class="flex-1">Collections</h2>
     </div>
+
     @if ($collections->isEmpty())
         <p class="card text-xs text-grey">There are no collections</p>
     @else
-    <div class="card flush dossier">
-        <div class="dossier-table-wrapper">
-            <table class="dossier">
-                <tbody>
-                    @foreach ($collections as $collection)
-                    <tr>
-                        <td class="cell-title first-cell">
-                            <span class="column-label">Title</span>
-                            <a class="" href="{{ route('seopro.collections.edit', ['collection' => $collection->path()]) }}">
-                                {{ $collection->title() }}
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="card flush dossier">
+            <div class="dossier-table-wrapper">
+                <table class="dossier">
+                    <tbody>
+                        @foreach ($collections as $collection)
+                        <tr>
+                            <td class="cell-title first-cell">
+                                <span class="column-label">Title</span>
+                                <a class="" href="{{ route('seopro.collections.edit', ['collection' => $collection->path()]) }}">
+                                    {{ $collection->title() }}
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     @endif
 
     <div class="flex items-center mb-1">
         <h2 class="flex-1">Taxonomies</h2>
     </div>
+
     @if ($taxonomies->isEmpty())
         <p class="card text-xs text-grey">There are no taxonomies</p>
     @else
-    <div class="card flush dossier">
-        <div class="dossier-table-wrapper">
-            <table class="dossier">
-                <tbody>
-                    @foreach ($taxonomies as $taxonomy)
-                    <tr>
-                        <td class="cell-title first-cell">
-                            <span class="column-label">Taxonomies</span>
-                            <a class="" href="{{ route('seopro.taxonomies.edit', ['taxonomy' => $taxonomy->path()]) }}">
-                                {{ $taxonomy->title() }}
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="card flush dossier">
+            <div class="dossier-table-wrapper">
+                <table class="dossier">
+                    <tbody>
+                        @foreach ($taxonomies as $taxonomy)
+                        <tr>
+                            <td class="cell-title first-cell">
+                                <span class="column-label">Taxonomies</span>
+                                <a class="" href="{{ route('seopro.taxonomies.edit', ['taxonomy' => $taxonomy->path()]) }}">
+                                    {{ $taxonomy->title() }}
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     @endif
 
 @stop
