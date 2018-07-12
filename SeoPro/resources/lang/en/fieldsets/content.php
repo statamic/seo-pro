154406d2b382fd21e05318e77e3ce56eae451343
Jack McDade <jack@jackmcdade.com>
2018-07-12 14:52:26 -0400

    string some letters together

--- SeoPro/resources/lang/en/fieldsets/content.php
+++ SeoPro/resources/lang/en/fieldsets/content.php
@@ -3,6 +3,18 @@
 return [
 
     'title' => 'Meta Title',
-    'title_instruct' => 'Meta title instructions.',
+    'title_instruct' => 'Every URL in your site should have a unique Meta Title, ideally less than 60 characters long.',
+
+    'description' => 'Meta Description',
+    'description_instruct' => 'Every URL in your site should have a unique Meta Description, ideally less than 160 characters long.',
+
+    'image' => 'Social Image',
+    'image_instruct' => 'This image is used as a social network preview image.',
+
+    'priority' => 'Sitemap: Priority',
+    'priority_instruct' => 'The priority of this URL relative to other URLs on your site. Valid values range from `0.0` to `1.0`.',
+
+    'change_frequency' => 'Sitemap: Change Frequency',
+    'change_frequency_instruct' => 'A hint to search engines on how frequently the page is likely to change.',
 
 ];
