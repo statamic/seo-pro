3b07655183d7aa6e66c240d149512b161e2ed5df
Marc Carlucci <mc@redpesto.com>
2018-10-09 10:39:31 +0100

    Added French translation

---
+++ SeoPro/resources/lang/fr/fieldsets/content.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'title' => 'Titre Meta',
+    'title_instruct' => 'Chaque URL de votre site doit avoir un titre unique, idéalement moins de 60 caractères.',
+
+    'description' => 'Description Meta',
+    'description_instruct' => 'Chaque URL de votre site doit avoir une description méta unique, idéalement inférieure à 160 caractères.',
+
+    'image' => 'Image réseau social',
+    'image_instruct' => 'Cette image est utilisée comme image de prévisualisation de réseau social.',
+
+    'priority' => 'Sitemap: Priorité',
+    'priority_instruct' => 'La priorité de cette URL par rapport aux autres URL de votre site. Les valeurs valides vont de `0.0` à` 1.0`.',
+
+    'change_frequency' => 'Sitemap: Fréquence de changement',
+    'change_frequency_instruct' => 'Un indice pour les moteurs de recherche sur la fréquence de changement de la page.',
+
+];
---
+++ SeoPro/resources/lang/fr/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'site_defaults' => 'Site par défaut',
+    'section_defaults' => 'Section par défaut',
+    'humans_txt' => 'Humans.txt',
+    'reports' => 'Rapports',
+    'seo_reports' => 'Rapports SEO',
+    'generate_report' => 'Créer un Rapport',
+    'back_to_reports' => 'Retour au rapports',
+    'report_is_being_generated' => 'Votre rapport est en cours de création.',
+    'generated' => 'Créé',
+    'page_details' => 'Details de la page',
+    'report_no_results_text' => 'Exécuter un rapport sur votre site pour voir comment il est configuré et apprendre comment l\'améliorer.',
+    'generate_your_first_report' => 'Créer votre premier rapport',
+    'latest_report_score' => 'Dernier score du rapport',
+    'source_suggest_placeholder' => 'Sélectionnez ou entrez un nom de champ contenant la valeur.',
+    'no_collections' => 'Il n\'y a pas de collections.',
+    'no_taxonomies' => 'Il n\'y a pas de taxonomies.',
+
+];
