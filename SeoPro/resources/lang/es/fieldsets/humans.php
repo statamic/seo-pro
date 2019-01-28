b053dbf9475635887a63d567436c61df94304f9d
Tomás Soler <tomas@tjsoler.me>
2019-01-28 16:13:29 +0100

    translates the plugin to spanish.

---
+++ SeoPro/resources/lang/es/fieldsets/humans.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'enabled' => 'Habilitado',
+    'enabled_instruct' => 'Humans.txt es una iniciativa para conocer a la gente detrás del sitio web.',
+
+    'content' => 'Contenido',
+    'content_instruct' => 'Lo siguiente es un fichero TXT generado que contiene información sobre las diferentes personas que han contribuído a la construcción del sitio.',
+
+];
---
+++ SeoPro/resources/lang/es/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'asset_container_instruct' => "El contenedor de imagenes es usado para cualquiera de los campos de imagen de SEO Pro.",
+
+    'og_image_dimensions' => 'Tamaños de imagen de OpenGraph',
+    'og_image_dimensions_instruct' => 'De acuerdo a <a href="https://www.h3xed.com/web-and-internet/how-to-use-og-image-meta-tag-facebook-reddit" target="_blank">este artículo</a>, el tamaño de imagen idal para las etiquetas de imagen Open Graph de Facebook y Reddit es 1200x1200 pixels.',
+
+    'open_graph_image_width' => 'Ancho',
+    'open_graph_image_width_instruct' => 'En pixels',
+
+    'open_graph_image_height' => 'Alto',
+    'open_graph_image_height_instruct' => 'En pixels',
+
+    'sitemap_enabled' => 'Habilitado',
+    'sitemap_enabled_instruct' => 'Si está habilitado, un xml de sitemp estará disponible en la URL definida abajo.',
+
+    'sitemap_url' => 'URL del Sitemap',
+    'sitemap_url_instruct' => "La localizacion del sitemap relativa a la raiz de tu página de home en el idioma por defecto.",
+
+    'sitemap_cache_length' => 'Duración de Cache',
+    'sitemap_cache_length_instruct' => 'Cuanto tiempo debería ser cacheado el Sitemap, en minutos.'
+
+];
