765b5289765c54e43217bbf3f0d62a9abbefc942
Vitor Pinho <eu@vitorpinho.com>
2019-02-09 15:28:12 +0100

    PORTUGUESE translation v1.1.2
    settings.php traduzido
    
    messages.php traduzido
    
    defaults.php traduzido
    
    content.php traduzido
    
    defaults.php traduzido
    
    humans.php traduzido
    
    sections.php traduzido

---
+++ SeoPro/resources/lang/pt/fieldsets/sections.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    'enabled' => 'Ativado',
+    'enabled_instruct' => 'Disabling this section will exclude it from reports and the sitemap, and prevent anything from being rendered through the template tag.',
+
+    'meta_section' => 'Meta Dados',
+
+    'title' => 'Meta Título',
+    'title_instruct' => "Pick an existing field to set as this section's **default** meta title.",
+
+    'description' => 'Meta Descrição',
+    'description_instruct' => "Escolha um campo existente para definir como meta descrição **padrão** da seção.",
+
+    'og_section' => 'Open Graph',
+    'og_section_instruct' => 'Nós geramos automaticamente a maioria dos campos do Open Graph a partir dos seus metadados e configuração do site.',
+
+    'image' => 'Imagem',
+    'image_instruct' => 'Escolha um campo de imagem padrão para representar cada URL quando partilhado nas redes sociais.',
+
+    'sitemap_section' => 'Site Map',
+
+    'sitemap' => 'Ativado',
+    'sitemap_instruct' => 'Se desativado, os itens desta seção não aparecerão no sitemap.',
+
+    'show_future' => 'Mostrar Entradas Futuras',
+    'show_future_instruct' => 'Se desativado, as entradas com data futura não aparecerão no sitemap.',
+
+    'show_past' => 'Mostrar Entradas Anteriores',
+    'show_past_instruct' => 'Se desativado, as entradas com datas anteriores não aparecerão no sitemap.',
+
+    'priority' => 'Prioridade',
+    'priority_instruct' => 'A prioridade deste URL em relação a outros URLs no seu site. Os valores válidos variam de `0.0` a `1.0`.',
+
+    'change_frequency' => 'Frequência de Mudança',
+    'change_frequency_instruct' => 'Uma dica para os mecanismos de pesquisa sobre a frequência com que se pode alterar uma página.',
+
+];
