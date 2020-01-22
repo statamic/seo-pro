0e8147b863af7792a0eb109dde32b87fbb386359
Jason Varga <jason@pixelfear.com>
2020-01-22 17:03:56 -0500

    Explicitly merge section values ...
    By setting the seo value on an entry/page, it completely overrides the value from the context. We need it to be merged.
    
    Ref #80

--- SeoPro/SeoProTags.php
+++ SeoPro/SeoProTags.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\Addons\SeoPro;
 
+use ReflectionClass;
 use Statamic\API\File;
 use Statamic\API\Parse;
 use Statamic\Extend\Tags;
@@ -20,11 +21,27 @@ class SeoProTags extends Tags
 
     public function metaData()
     {
+        $current = array_get($this->context, 'page_object');
+        $sectionDefaults = $this->getSectionDefaults($current);
+        $currentValues = array_get($this->context, 'seo', []);
+
         return (new TagData)
             ->with(Settings::load()->get('defaults'))
-            ->with(array_get($this->context, 'seo', []))
-            ->withCurrent(array_get($this->context, 'page_object'))
+            ->with(array_merge($sectionDefaults, $currentValues))
+            ->withCurrent($current)
             ->get();
+    }
+
+    private function getSectionDefaults($obj)
+    {
+        // The cascadingData method is exactly what's needed here, but it's
+        // protected. Rather than update core and need to require the
+        // latest Statamic, just cheat with some reflection.
+        $ref = new \ReflectionClass($obj);
+        $met = $ref->getMethod('cascadingData');
+        $met->setAccessible(true);
+        $data = $met->invokeArgs($obj, []);
+        return array_get($data, 'seo', []);
     }
 
     public function dumpMetaData()
