8f617e08c78cd00bb93b029ad8e6ac5993776f6b
Jesse Leite <jesseleite@gmail.com>
2020-07-17 17:40:15 -0400

    Remove defaults from config.

--- config/seo-pro.php
+++ config/seo-pro.php
@@ -2,21 +2,6 @@
 
 return [
 
-    'defaults' => [
-        'title' => '@seo:title',
-        'description' => '@seo:content',
-        'site_name' => 'Site Name',
-        'site_name_position' => 'after',
-        'site_name_separator' => '|',
-        'priority' => '0.5',
-        'change_frequency' => 'monthly',
-        // 'image' => '@seo:hero_image',
-        // 'robots' => ['nofollow', 'noindex'],
-        // 'twitter_site' => 'twitter_handle',
-        // 'bing_verification' => 'verification_code',
-        // 'google_verification' => 'verification_code',
-    ],
-
     'assets' => [
         'container' => null,
         'open_graph_preset' => [
