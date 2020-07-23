67fa3a4f74e138adba3c201061e85df69161b785
Jesse Leite <jesseleite@gmail.com>
2020-07-23 14:02:51 -0400

    Remove old JS builds.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,3 @@ let mix = require('laravel-mix');
 mix
     .js('resources/js/cp.js', 'resources/dist/js')
     .setPublicPath('resources/dist');
-
-// mix.js('SeoPro/resources/assets/src/js/scripts.js', 'SeoPro/resources/assets/js');
-// mix.js('SeoPro/resources/assets/src/js/fieldtype.js', 'SeoPro/resources/assets/js');
