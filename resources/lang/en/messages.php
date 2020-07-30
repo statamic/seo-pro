88c9076e518d56d431d7f435b9597f67a791f197
Jesse Leite <jesseleite@gmail.com>
2020-07-30 16:21:30 -0400

    This was being used by widget.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -16,6 +16,7 @@ return [
     'report_is_being_generated' => 'Your report is being generated.',
     'generated' => 'Generated',
     'page_details' => 'Page Details',
+    'report_no_results_text' => 'Run a report on your site to see how well it has been configured and learn ways for it to be improved.',
     'generate_your_first_report' => 'Generate your first report',
     'latest_report_score' => 'Latest Report Score',
     'source_suggest_placeholder' => 'Select or type a field name containing the value.',
