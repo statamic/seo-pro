3d2561368efede259cb9b088bf6e420756d383ce
Jesse Leite <jesseleite@gmail.com>
2020-08-15 12:44:18 -0400

    Sort by actual report date/timestamp, rather than folder path string.

--- src/Reporting/Report.php
+++ src/Reporting/Report.php
@@ -233,11 +233,11 @@ class Report implements Arrayable, Jsonable
             ->map(function ($path) {
                 return (int) pathinfo($path)['filename'];
             })
-            ->sort()
-            ->reverse()
             ->map(function ($id) {
                 return static::find($id);
             })
+            ->sortByDesc
+            ->date()
             ->values();
     }
 
