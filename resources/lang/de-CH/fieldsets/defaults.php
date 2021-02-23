01af5853a21e13203919ca5056e0572d10e531cd
helloDanuk <hello@danielkaufmann.ch>
2021-02-23 01:01:31 +0100

    Betterify («Website» according to duden.de and «Meta-…»)

--- resources/lang/de-CH/fieldsets/defaults.php
+++ resources/lang/de-CH/fieldsets/defaults.php
@@ -3,52 +3,52 @@
 return [
 
     'meta_section' => 'Metadaten ',
-    'meta_section_instruct' => 'Jede URL deiner Webseite sollte einen Metatitel und eine Metabeschreibung haben, die eindeutig sind.',
+    'meta_section_instruct' => 'Jede URL deiner Website sollte einen Meta-Titel und eine Meta-Beschreibung haben, die eindeutig sind.',
 
-    'title' => 'Meta Titel',
-    'title_instruct' => 'Wähle für den Metatitel ein vorhandenes Feld als Standard. Jede Sammlung und Taxonomie kann auch ihre eigene Einstellung haben.',
+    'title' => 'Meta-Titel',
+    'title_instruct' => 'Wähle für den Meta-Titel ein vorhandenes Feld als Standard. Jede Sammlung und Taxonomie kann auch ihre eigene Einstellung haben.',
 
-    'description' => 'Meta Beschreibung',
-    'description_instruct' => 'Wähle für die Metabeschreibung ein vorhandenes Feld als Standard. Jede Sammlung und Taxonomie kann auch ihre eigene Einstellung haben.',
+    'description' => 'Meta-Beschreibung',
+    'description_instruct' => 'Wähle für die Meta-Beschreibung ein vorhandenes Feld als Standard. Jede Sammlung und Taxonomie kann auch ihre eigene Einstellung haben.',
 
-    'site_name' => 'Webseitenname',
-    'site_name_instruct' => "Es ist üblich, den Namen der Webseite in die Seitentitel einzufügen.",
+    'site_name' => 'Name der Website',
+    'site_name_instruct' => "Es ist üblich, den Namen der Website in die Seitentitel einzufügen.",
 
-    'site_name_position' => 'Webseitenname: Position',
+    'site_name_position' => 'Name der Website: Position',
     'site_name_position_instruct' => 'Ob vor oder nach dem Titel, bleibt dir überlassen.',
 
-    'site_name_separator' => 'Webseitenname: Trennzeichen',
-    'site_name_separator_instruct' => 'Das (oder die) Zeichen zwischen Titel und Webseitenname.',
+    'site_name_separator' => 'Name der Website: Trennzeichen',
+    'site_name_separator_instruct' => 'Das (oder die) Zeichen zwischen Titel und dem Namen der Website.',
 
     'canonical_url' => 'Kanonische URL',
     'canonical_url_instruct' => 'Wähle für die kanonische URL ein vorhandenes Feld als Standard. Jede Sammlung und Taxonomie kann auch ihre eigene Einstellung haben.',
 
     'robots' => 'Robots',
-    'robots_instruct' => 'Wähle Optionen für das Robots Meta Tag. `noindex` verhindert, dass die Seite von Suchmaschinen indexiert wird. `nofollow` verhindert, dass Suchmaschinen Links crawlen.',
+    'robots_instruct' => 'Wähle Optionen für das Robots Meta-Tag. `noindex` verhindert, dass die Seite von Suchmaschinen indexiert wird. `nofollow` verhindert, dass Suchmaschinen Links crawlen.',
 
     'image_section' => 'Open Graph',
-    'image_section_instruct' => 'Wir erstellen die meisten Open-Graph Felder automatisch aus deinen Metadaten und der Webseiten-Konfiguration.',
+    'image_section_instruct' => 'Wir erstellen die meisten Open-Graph Felder automatisch aus deinen Metadaten und der Website-Konfiguration.',
 
     'image' => 'Bild',
     'image_instruct' => 'Wähle ein Feld für ein Bild, das jede URL beim Teilen in sozialen Netzwerken als Standard anzeigen soll.',
 
     'social_section' => 'Twitter',
-    'social_section_instruct' => 'Wir erstellen die meisten Twitter-Card Felder automatisch aus deinen Metadaten und der Webseiten-Konfiguration.',
+    'social_section_instruct' => 'Wir erstellen die meisten Twitter-Card Felder automatisch aus deinen Metadaten und der Website-Konfiguration.',
 
     'twitter_handle' => 'Twitter-Handle',
-    'twitter_handle_instruct' => 'Wähle einen voreingestellten Twitter-Handle, der zur Webseite passt.',
+    'twitter_handle_instruct' => 'Wähle einen voreingestellten Twitter-Handle, der zur Website passt.',
 
     'sitemap_section' => 'Sitemap',
     'sitemap_section_instruct' => "Wähle deine Voreinstellungen für die Sitemap. Wenn du unsicher sind, lasse die Felder zugunsten der üblichen Standardwerte leer.",
 
     'priority' => 'Priorität',
-    'priority_instruct' => 'Die Wichtigkeit dieser URL relativ zu anderen URLs auf deiner Webseite. Gültige Werte reichen von `0.0` bis `1.0`.',
+    'priority_instruct' => 'Die Wichtigkeit dieser URL relativ zu anderen URLs auf deiner Website. Gültige Werte reichen von `0.0` bis `1.0`.',
 
     'change_frequency' => 'Frequenz',
     'change_frequency_instruct' => 'Ein Hinweis für Suchmaschinen, wie häufig sich die Seite wahrscheinlich ändern wird.',
 
     'search_section' => 'Suchmaschinen',
-    'search_section_instruct' => "Durch die Verifizierung deiner Webseite bei beliebten Suchmaschinen kannst du prüfen, wie gut deine Webseite von diesen gecrawlt wird.",
+    'search_section_instruct' => "Durch die Verifizierung deiner Website bei beliebten Suchmaschinen kannst du prüfen, wie gut deine Website von diesen gecrawlt wird.",
 
     'bing_verification' => 'Bing Verifizierungscode',
     'bing_verification_instruct' => 'Hol dir deinen Bing-Verifizierungscode in den [Bing Webmaster Tools](https://www.bing.com/toolbox/webmaster).',
--- resources/lang/de-CH/fieldsets/sections.php
+++ resources/lang/de-CH/fieldsets/sections.php
@@ -7,29 +7,29 @@ return [
 
     'meta_section' => 'Metadaten',
 
-    'title' => 'Meta Titel',
-    'title_instruct' => "Wähle für die Metatitel in diesem Bereich ein vorhandenes Feld als Standard.",
+    'title' => 'Meta-Titel',
+    'title_instruct' => "Wähle für die Meta-Titel in diesem Bereich ein vorhandenes Feld als Standard.",
 
-    'description' => 'Meta Beschreibung',
-    'description_instruct' => "Wähle für die Metabeschreibungen in diesem Bereich ein vorhandenes Feld als Standard.",
+    'description' => 'Meta-Beschreibung',
+    'description_instruct' => "Wähle für die Meta-Beschreibungen in diesem Bereich ein vorhandenes Feld als Standard.",
 
-    'site_name' => 'Webseitenname',
-    'site_name_instruct' => 'Optional den Webseitenname in diesem Bereich deaktivieren.',
+    'site_name' => 'Name der Website',
+    'site_name_instruct' => 'Optional den Namen der Website in diesem Bereich deaktivieren.',
 
-    'site_name_position' => 'Webseitenname: Position',
+    'site_name_position' => 'Name der Website: Position',
     'site_name_position_instruct' => 'Optional die Position in diesem Bereich anpassen.',
 
-    'site_name_separator' => 'Webseitenname: Trennzeichen',
-    'site_name_separator_instruct' => 'Optional das Trennzeichen zwischen Metatitel und Webseitenname in diesem Bereich anpassen.',
+    'site_name_separator' => 'Name der Website: Trennzeichen',
+    'site_name_separator_instruct' => 'Optional das Trennzeichen zwischen Meta-Titel und dem Namen der Website in diesem Bereich anpassen.',
 
     'canonical_url' => 'Kanonische URL',
     'canonical_url_instruct' => "Wähle für die kanonischen URLs in diesem Bereich ein vorhandenes Feld als Standard.",
 
     'robots' => 'Robots',
-    'robots_instruct' => 'Wähle Optionen für das Robots Meta Tag. `noindex` verhindert, dass die Seite von Suchmaschinen indexiert wird. `nofollow` verhindert, dass Suchmaschinen Links crawlen.',
+    'robots_instruct' => 'Wähle Optionen für das Robots Meta-Tag. `noindex` verhindert, dass die Seite von Suchmaschinen indexiert wird. `nofollow` verhindert, dass Suchmaschinen Links crawlen.',
 
     'og_section' => 'Open Graph',
-    'og_section_instruct' => 'Wir erstellen die meisten Open-Graph Felder automatisch aus deinen Metadaten und der Webseitenkonfiguration.',
+    'og_section_instruct' => 'Wir erstellen die meisten Open-Graph Felder automatisch aus deinen Metadaten und der Website-Konfiguration.',
 
     'image' => 'Bild',
     'image_instruct' => 'Wähle ein Feld für ein Bild, das jede URL in diesem Bereich beim Teilen in sozialen Netzwerken als Standard anzeigen soll.',
@@ -49,7 +49,7 @@ return [
     'show_past_instruct' => 'Ist diese Option deaktiviert, werden in der Vergangenheit liegende Einträge nicht in der Sitemap angezeigt.',
 
     'priority' => 'Sitemap: Priorität',
-    'priority_instruct' => 'Die Wichtigkeit dieser URLs relativ zu anderen URLs auf deiner Webseite. Gültige Werte reichen von `0.0` bis `1.0`.',
+    'priority_instruct' => 'Die Wichtigkeit dieser URLs relativ zu anderen URLs auf deiner Website. Gültige Werte reichen von `0.0` bis `1.0`.',
 
     'change_frequency' => 'Sitemap: Frequenz',
     'change_frequency_instruct' => 'Ein Hinweis für Suchmaschinen, wie häufig sich die Seiten in diesem Bereich wahrscheinlich ändern werden.',
