dd48998f2b17eae7ef9964cc9b6f2c8b6efa5811
Jesse Leite <jesseleite@gmail.com>
2022-11-04 18:20:38 -0400

    Fix missing view context with blade layout components.

--- src/Directives/SeoProDirective.php
+++ src/Directives/SeoProDirective.php
@@ -2,12 +2,27 @@
 
 namespace Statamic\SeoPro\Directives;
 
+use Facades\Statamic\View\Cascade;
 use Statamic\SeoPro\Tags\SeoProTags;
 
 class SeoProDirective extends SeoProTags
 {
     public function renderTag($tag, $context)
     {
+        if ($this->isUsingBladeComponents($context)) {
+            $context = $this->getContextFromCascade();
+        }
+
         return $this->setContext($context)->$tag();
+    }
+
+    protected function isUsingBladeComponents($context)
+    {
+        return ! isset($context['current_template']);
+    }
+
+    protected function getContextFromCascade()
+    {
+        return Cascade::instance()->toArray();
     }
 }
