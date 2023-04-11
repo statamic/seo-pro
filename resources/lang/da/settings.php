331d5176790caa39332c23fb59750ec889a8df89
René D <rene@ttmedia.dk>
2023-04-11 15:13:07 +0200

    Danish translations

---
+++ resources/lang/da/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'asset_container_instruct' => 'Arkivbeholderen der bruges til alle SEO Pro’s billedefelter.',
+
+    'og_image_dimensions' => 'OpenGraph Billedstørrelse',
+    'og_image_dimensions_instruct' => 'Ifølge <a href="https://www.h3xed.com/web-and-internet/how-to-use-og-image-meta-tag-facebook-reddit" target="_blank">denne artikel</a> er den ideelle billedstørrelse til Facebook og Reddit Open Graph-tags 1200x1200 pixels.',
+
+    'open_graph_image_width' => 'Bredde',
+    'open_graph_image_width_instruct' => 'I pixels',
+
+    'open_graph_image_height' => 'Højde',
+    'open_graph_image_height_instruct' => 'I pixels',
+
+    'sitemap_enabled' => 'Aktiveret',
+    'sitemap_enabled_instruct' => 'Hvis aktiveret, vil en sitemap xml-fil kunne ses på URL’en, der er defineret nedenfor.',
+
+    'sitemap_url' => 'Sitemap URL',
+    'sitemap_url_instruct' => 'Placeringen af sitemap i forhold til roden af dit standard sprogs forside.',
+
+    'sitemap_cache_length' => 'Cache længde',
+    'sitemap_cache_length_instruct' => 'Hvor lang tid sitemap skal cachet, i minutter.',
+
+];
