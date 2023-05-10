a0d6621a2b5e64c8026023608e842c8bad97e4d9
Jesse Leite <jesseleite@gmail.com>
2023-05-10 12:10:30 -0400

    Fix controller namespace bug introduced in v4.

--- routes/cp.php
+++ routes/cp.php
@@ -1,16 +1,18 @@
 <?php
 
-Route::view('seo-pro', 'seo-pro::index')->name('seo-pro.index');
-Route::get('seo-pro/reports', 'ReportController@index')->name('seo-pro.reports.index');
-Route::post('seo-pro/reports', 'ReportController@store')->name('seo-pro.reports.store');
-Route::get('seo-pro/reports/{seo_pro_report}', 'ReportController@show')->name('seo-pro.reports.show');
-Route::delete('seo-pro/reports/{seo_pro_report}', 'ReportController@destroy')->name('seo-pro.reports.destroy');
+use Statamic\SeoPro\Http\Controllers;
 
-Route::get('seo-pro/site-defaults/edit', 'SiteDefaultsController@edit')->name('seo-pro.site-defaults.edit');
-Route::post('seo-pro/site-defaults', 'SiteDefaultsController@update')->name('seo-pro.site-defaults.update');
+Route::view('seo-pro', 'seo-pro::index')->name('seo-pro.index');
+Route::get('seo-pro/reports', [Controllers\ReportController::class, 'index'])->name('seo-pro.reports.index');
+Route::post('seo-pro/reports', [Controllers\ReportController::class, 'store'])->name('seo-pro.reports.store');
+Route::get('seo-pro/reports/{seo_pro_report}', [Controllers\ReportController::class, 'show'])->name('seo-pro.reports.show');
+Route::delete('seo-pro/reports/{seo_pro_report}', [Controllers\ReportController::class, 'destroy'])->name('seo-pro.reports.destroy');
+
+Route::get('seo-pro/site-defaults/edit', [Controllers\SiteDefaultsController::class, 'edit'])->name('seo-pro.site-defaults.edit');
+Route::post('seo-pro/site-defaults', [Controllers\SiteDefaultsController::class, 'update'])->name('seo-pro.site-defaults.update');
 
 Route::view('seo-pro/section-defaults', 'seo-pro::sections')->name('seo-pro.section-defaults.index');
-Route::get('seo-pro/section-defaults/collections/{seo_pro_collection}/edit', 'CollectionDefaultsController@edit')->name('seo-pro.section-defaults.collections.edit');
-Route::post('seo-pro/section-defaults/collections/{seo_pro_collection}', 'CollectionDefaultsController@update')->name('seo-pro.section-defaults.collections.update');
-Route::get('seo-pro/section-defaults/taxonomies/{seo_pro_taxonomy}/edit', 'TaxonomyDefaultsController@edit')->name('seo-pro.section-defaults.taxonomies.edit');
-Route::post('seo-pro/section-defaults/taxonomies/{seo_pro_taxonomy}', 'TaxonomyDefaultsController@update')->name('seo-pro.section-defaults.taxonomies.update');
+Route::get('seo-pro/section-defaults/collections/{seo_pro_collection}/edit', [Controllers\CollectionDefaultsController::class, 'edit'])->name('seo-pro.section-defaults.collections.edit');
+Route::post('seo-pro/section-defaults/collections/{seo_pro_collection}', [Controllers\CollectionDefaultsController::class, 'update'])->name('seo-pro.section-defaults.collections.update');
+Route::get('seo-pro/section-defaults/taxonomies/{seo_pro_taxonomy}/edit', [Controllers\TaxonomyDefaultsController::class, 'edit'])->name('seo-pro.section-defaults.taxonomies.edit');
+Route::post('seo-pro/section-defaults/taxonomies/{seo_pro_taxonomy}', [Controllers\TaxonomyDefaultsController::class, 'update'])->name('seo-pro.section-defaults.taxonomies.update');
