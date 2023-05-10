fc087a0b9b29d38ac2d7ee2905719244b9dff756
Jesse Leite <jesseleite@gmail.com>
2023-05-10 17:31:58 -0400

    Fix styling regressions (#273)
    Co-authored-by: Jack McDade <jack@jackmcdade.com>

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,11 +1,11 @@
 <div class="card p-0 overflow-hidden">
-    <div class="flex justify-between items-center p-2 mb-2">
+    <div class="flex justify-between items-center p-4 mb-4">
         <h2>SEO Pro</h2>
         <a href="{{ cp_route('seo-pro.reports.index') }}" class="text-blue hover:text-blue-dark text-sm">
             {{ __('seo-pro::messages.reports') }}
         </a>
     </div>
-    <div class="flex flex-col h-full -mt-5 items-center justify-center p-2 mb-2">
+    <div class="flex flex-col h-full -mt-10 items-center justify-center p-4 mb-4">
         @if ($report)
             <div class="text-4xl leading-tight font-light
                 @if ($report->score() < 70) text-red
@@ -18,8 +18,8 @@
                 {{ __('seo-pro::messages.latest_report_score') }}
             </p>
         @else
-            <div class="text-center pt-3 pb-1">
-                <p class="text-grey text-sm mb-3">
+            <div class="text-center pt-6 pb-2">
+                <p class="text-grey text-sm mb-6">
                     {{ __('seo-pro::messages.report_no_results_text') }}
                 </p>
                 <a href="{{ cp_route('seo-pro.reports.index') }}" class="btn btn-default">
