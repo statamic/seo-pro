8cc32ae5a4620e0c5b4784b651d069e0fc835730
René Dyja <rene@ttmedia.dk>
2023-05-17 02:49:54 +0200

    Danish translations (#267)

---
+++ resources/lang/da/fieldsets/content.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    'enabled' => 'Aktiveret',
+    'enabled_instruct' => 'Hvis du deaktiverer dette element, vil det blive udelukket fra rapporter og sitemap, og forhindre at noget bliver vist gennem skabelon-tagget.',
+
+    'title' => 'Meta Titel',
+    'title_instruct' => 'Hver URL på din hjemmeside bør have en unik Meta Titel, ideelt set mindre end 60 tegn lang.',
+
+    'description' => 'Meta Beskrivelse',
+    'description_instruct' => 'Hver URL på din hjemmeside bør have en unik Meta Beskrivelse, ideelt set mindre end 160 tegn lang.',
+
+    'site_name' => 'Webstedsnavn',
+    'site_name_instruct' => 'Deaktiver webstedsnavnet for denne side, hvis det ønskes.',
+
+    'site_name_position' => 'Webstedsnavn Position',
+    'site_name_position_instruct' => 'Justér eventuelt positionen for webstedsnavnet på denne side.',
+
+    'site_name_separator' => 'Webstedsnavn Separator',
+    'site_name_separator_instruct' => 'Justér eventuelt separator for webstedsnavnet på denne side.',
+
+    'canonical_url' => 'Canonical URL',
+    'canonical_url_instruct' => 'Hver URL på din hjemmeside bør have en unik canonical URL.',
+
+    'robots' => 'Robots',
+    'robots_instruct' => 'Vælg indstillinger for robots meta tagget. noindex forhindrer siden i at blive indekseret af søgemaskiner. nofollow forhindrer søgemaskiner i at krybe links.',
+
+    'image' => 'Socialt billede',
+    'image_instruct' => 'Dette billede bruges som forhåndsvisning af sociale netværk.',
+
+    'twitter_handle' => 'Twitter Handle',
+    'twitter_handle_instruct' => 'Overstyr eventuelt Twitter håndtaget for denne side.',
+
+    'sitemap' => 'Sitemap',
+    'sitemap_instruct' => 'Hvis deaktiveret, vil dette element ikke blive vist i sitemap.',
+
+    'priority' => 'Sitemap: Prioritet',
+    'priority_instruct' => 'Prioriteten af denne URL i forhold til andre URLs på dit websted. Gyldige værdier ligger mellem `0,0` og `1,0`.',
+
+    'change_frequency' => 'Sitemap: Frekvens af ændringer',
+    'change_frequency_instruct' => 'Et hint til søgemaskiner om, hvor ofte siden sandsynligvis vil ændre sig.',
+
+];
---
+++ resources/lang/da/fieldsets/sections.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'enabled' => 'Aktiveret',
+    'enabled_instruct' => 'Hvis denne sektion deaktiveres, vil den blive ekskluderet fra rapporter og sitemap, og forhindre noget i at blive vist gennem skabelontaggen.',
+
+    'meta_section' => 'Meta Data',
+
+    'title' => 'Meta Titel',
+    'title_instruct' => "Vælg et eksisterende felt som denne sektions **standard** meta titel.",
+
+    'description' => 'Meta Beskrivelse',
+    'description_instruct' => "Vælg et eksisterende felt som denne sektions **standard** meta beskrivelse.",
+
+    'site_name' => 'Webstedsnavn',
+    'site_name_instruct' => 'Deaktiver eventuelt webstedsnavnet for denne sektion.',
+
+    'site_name_position' => 'Webstedsnavn position',
+    'site_name_position_instruct' => 'Justér eventuelt positionen for webstedsnavnet i denne sektion.',
+
+    'site_name_separator' => 'Webstedsnavn separator',
+    'site_name_separator_instruct' => 'Justér eventuelt separationskarakteren for webstedsnavnet i denne sektion.',
+
+    'canonical_url' => 'Canonical URL',
+    'canonical_url_instruct' => "Vælg et eksisterende felt som denne sektions **standard** canonical URL.",
+
+    'robots' => 'Robots',
+    'robots_instruct' => 'Vælg indstillinger for robots meta-tagget. noindex forhindrer, at siden indekseres af søgemaskiner. nofollow forhindrer, at søgemaskinerne crawler links.',
+
+    'og_section' => 'Open Graph',
+    'og_section_instruct' => 'Vi genererer automatisk de fleste Open Graph-felter fra din meta data og websteds konfiguration.',
+
+    'image' => 'Billede',
+    'image_instruct' => 'Vælg et standardbilledefelt til at repræsentere hver URL i denne sektion, når det deles på sociale netværk.',
+
+    'twitter_handle' => 'Twitter brugernavn',
+    'twitter_handle_instruct' => 'Vælg et standard Twitter-brugernavn, der svarer til denne sektion.',
+
+    'sitemap_section' => 'Sitemap',
+
+    'sitemap' => 'Sitemap',
+    'sitemap_instruct' => 'Hvis deaktiveret, vil emner i denne sektion ikke blive vist i sitemap.',
+
+    'show_future' => 'Vis fremtidige indlæg',
+    'show_future_instruct' => 'Hvis deaktiveret, vil indlæg, der er dateret i fremtiden, ikke blive vist i sitemap.',
+
+    'show_past' => 'Vis tidligere indlæg',
+    'show_past_instruct' => 'Hvis deaktiveret, vil indlæg, der er dateret i fortiden, ikke blive vist i sitemap.',
+
+    'priority' => 'Prioritet',
+    'priority_instruct' => 'Prioriteten af disse URLs i forhold til andre URLs på dit websted. Gyldige værdier spænder fra `0,0` til `1,0`.',
+
+    'change_frequency' => 'Ændringsfrekvens',
+    'change_frequency_instruct' => 'En antydning til søgemaskiner om, hvor ofte siderne i denne sektion sandsynligvis vil ændre sig.',
+
+];
