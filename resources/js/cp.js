557acd6d023e3ece04d0db0252746560d8c43ff8
Jesse Leite <jesseleite@gmail.com>
2023-11-08 15:57:00 -0500

    Remove reports and listing vue components.

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,7 +1,7 @@
 import SeoProFieldtype from './components/fieldtypes/SeoProFieldtype.vue';
 import SourceFieldtype from './components/fieldtypes/SourceFieldtype.vue';
-import Reports from './components/reporting/Reports.vue';
+import StatusIcon from './components/reporting/StatusIcon.vue';
 
 Statamic.$components.register('seo_pro-fieldtype', SeoProFieldtype);
 Statamic.$components.register('seo_pro_source-fieldtype', SourceFieldtype);
-Statamic.$components.register('seo-reports', Reports);
+Statamic.$components.register('seo-status-icon', StatusIcon);
