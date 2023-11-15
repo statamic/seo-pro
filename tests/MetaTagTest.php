313e992c16ad32edc986e97e3ca6eef03eb215a8
Jesse Leite <jesseleite@gmail.com>
2023-11-15 15:49:25 -0500

    Add cascade hydration when using blade directive with custom route/controller implementation.

--- tests/MetaTagTest.php
+++ tests/MetaTagTest.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Statamic\Extensions\Pagination\LengthAwarePaginator as StatamicLengthAwarePaginator;
 use Statamic\Facades\Blink;
+use Statamic\Facades\Cascade as StatamicViewCacade;
 use Statamic\Facades\Collection;
 use Statamic\Facades\Config;
 use Statamic\Facades\Entry;
@@ -27,6 +28,14 @@ class MetaTagTest extends TestCase
                 'title' => 'The View',
                 'description' => 'A wonderful view!',
             ]);
+
+            Route::get('custom-get-route', function () {
+                StatamicViewCacade::hydrated(function ($cascade) {
+                    $cascade->set('title', 'Custom Route Entry Title');
+                });
+
+                return view('page');
+            });
         });
     }
 
@@ -744,6 +753,24 @@ EOT;
 
         $this->assertStringContainsString("<h1>{$viewType}</h1>", $content);
         $this->assertStringContainsString($this->normalizeMultilineString($expected), $content);
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider viewScenarioProvider
+     */
+    public function it_hydrates_cascade_on_custom_routes_using_blade_directive($viewType)
+    {
+        if ($viewType === 'antlers') {
+            $this->markTestSkipped();
+        }
+
+        $this->prepareViews($viewType);
+
+        $content = $this->get('/custom-get-route')->content();
+
+        $this->assertStringContainsString('<title>Custom Route Entry Title | Site Name</title>', $content);
     }
 
     protected function setCustomGlidePresetDimensions($app)
