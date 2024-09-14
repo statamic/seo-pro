2869f0403f86180eb29e9599ff572fec25e2c29d
John Koster <john@stillat.com>
2024-09-14 17:19:27 -0500

    Tidy up a bit

--- src/Commands/GenerateEmbeddingsCommand.php
+++ src/Commands/GenerateEmbeddingsCommand.php
@@ -10,25 +10,10 @@ class GenerateEmbeddingsCommand extends Command
 {
     use RunsInPlease;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'statamic:seo-pro:generate-embeddings';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Generates embeddings for entries.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle(EntryEmbeddingsRepository $vectors)
     {
         $this->line('Generating...');
--- src/Commands/GenerateKeywordsCommand.php
+++ src/Commands/GenerateKeywordsCommand.php
@@ -10,25 +10,10 @@ class GenerateKeywordsCommand extends Command
 {
     use RunsInPlease;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'statamic:seo-pro:generate-keywords';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Generates keywords for entries.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle(KeywordsRepository $keywords)
     {
         $this->line('Generating...');
