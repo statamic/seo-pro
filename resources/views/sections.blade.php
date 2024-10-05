8fd6f7c24a43949fb94c9ce3d79a2318e34292ae
Jesse Leite <jesseleite@gmail.com>
2024-10-04 22:38:27 -0400

    Wrap groups with `@if`.

--- resources/views/sections.blade.php
+++ resources/views/sections.blade.php
@@ -7,37 +7,41 @@
         <h1 class="flex-1">{{ __('seo-pro::messages.section_defaults') }}</h1>
     </div>
 
-    <h3 class="little-heading pl-0 mb-2">{{ __('Collections') }}</h3>
-    <div class="card p-0 mb-4">
-        <table class="data-table">
-            @foreach (Statamic\Facades\Collection::all() as $collection)
+    @if (Statamic\Facades\Collection::all()->count())
+        <h3 class="little-heading pl-0 mb-2">{{ __('Collections') }}</h3>
+        <div class="card p-0 mb-4">
+            <table class="data-table">
+                @foreach (Statamic\Facades\Collection::all() as $collection)
+                        <tr>
+                            <td>
+                                <div class="flex items-center">
+                                    <div class="w-4 h-4 mr-4">@cp_svg('icons/light/content-writing')</div>
+                                    <a href="{{ cp_route('seo-pro.section-defaults.collections.edit', $collection) }}">{{ $collection->title() }}</a>
+                                </div>
+                            </td>
+                        </tr>
+                @endforeach
+            </table>
+        </div>
+    @endif
+
+    @if (Statamic\Facades\Taxonomy::all()->count())
+        <h3 class="little-heading pl-0 mb-2">{{ __('Taxonomies') }}</h3>
+        <div class="card p-0 mb-4">
+            <table class="data-table">
+                @foreach (Statamic\Facades\Taxonomy::all() as $taxonomy)
                     <tr>
                         <td>
                             <div class="flex items-center">
-                                <div class="w-4 h-4 mr-4">@cp_svg('icons/light/content-writing')</div>
-                                <a href="{{ cp_route('seo-pro.section-defaults.collections.edit', $collection) }}">{{ $collection->title() }}</a>
+                                <div class="w-4 h-4 mr-4">@cp_svg('icons/light/tags')</div>
+                                <a href="{{ cp_route('seo-pro.section-defaults.taxonomies.edit', $taxonomy) }}">{{ $taxonomy->title() }}</a>
                             </div>
                         </td>
                     </tr>
-            @endforeach
-        </table>
-    </div>
-
-    <h3 class="little-heading pl-0 mb-2">{{ __('Taxonomies') }}</h3>
-    <div class="card p-0 mb-4">
-        <table class="data-table">
-            @foreach (Statamic\Facades\Taxonomy::all() as $taxonomy)
-                <tr>
-                    <td>
-                        <div class="flex items-center">
-                            <div class="w-4 h-4 mr-4">@cp_svg('icons/light/tags')</div>
-                            <a href="{{ cp_route('seo-pro.section-defaults.taxonomies.edit', $taxonomy) }}">{{ $taxonomy->title() }}</a>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
+                @endforeach
+            </table>
+        </div>
+    @endif
 
     @include('statamic::partials.docs-callout', [
         'topic' => 'SEO Pro',
