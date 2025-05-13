d83292a5c908dcebecfbdf1e1b45ea43dd1f4cbc
Jesse Leite <jesseleite@gmail.com>
2025-05-13 13:35:06 -0400

    Render `errors` in seo fieldtype.

--- resources/js/components/fieldtypes/SeoProFieldtype.vue
+++ resources/js/components/fieldtypes/SeoProFieldtype.vue
@@ -7,6 +7,7 @@
             :value="value[field.handle]"
             :meta="meta.meta[field.handle]"
             :read-only="! field.localizable"
+            :errors="errors(field.handle)"
             class="form-group"
             @meta-updated="metaUpdated(field.handle, $event)"
             @focus="$emit('focus')"
@@ -32,6 +33,8 @@ export default {
 
     mixins: [Fieldtype],
 
+    inject: ['storeName'],
+
     computed: {
         fields() {
             return _.chain(this.meta.fields)
@@ -53,7 +56,13 @@ export default {
             Vue.set(seoValue, handle, value);
 
             this.update(seoValue);
-        }
+        },
+
+        errors(handle) {
+            const state = this.$store.state.publish[this.storeName];
+            if (!state) return [];
+            return state.errors['seo.'+handle] || [];
+        },
     },
 
 }
