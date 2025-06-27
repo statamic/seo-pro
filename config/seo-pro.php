f465a7cd5bc3dab98b668f921a66a7b651ab9109
Jesse Leite <jesseleite@gmail.com>
2025-06-27 17:30:05 -0400

    Add config to enforce trailing slashes on sitemap.

--- config/seo-pro.php
+++ config/seo-pro.php
@@ -22,6 +22,7 @@ return [
         'enabled' => true,
         'url' => 'sitemap.xml',
         'expire' => 60,
+        'enforce_trailing_slashes' => false,
         'pagination' => [
             'enabled' => false,
             'url' => 'sitemap_{page}.xml',
