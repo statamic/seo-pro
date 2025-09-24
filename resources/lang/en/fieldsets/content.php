4423085d65eae28abda58b84df7c1b4f593a54b1
Ryan Mitchell <ryan@thoughtcollective.com>
2025-09-24 08:49:27 +0100

    twitter -> x

--- resources/lang/en/fieldsets/content.php
+++ resources/lang/en/fieldsets/content.php
@@ -32,8 +32,8 @@ return [
     'og_title' => 'Opengraph Title',
     'og_title_instruct' => 'Pick an existing field or custom value to set as your opengraph title.',
 
-    'twitter_handle' => 'Twitter Handle',
-    'twitter_handle_instruct' => 'Optionally override the twitter handle for this page.',
+    'twitter_handle' => 'X Handle',
+    'twitter_handle_instruct' => 'Optionally override the X handle for this page.',
 
     'twitter_title' => 'X Card Title',
     'twitter_title_instruct' => 'Pick an existing field or custom value to set as your X card title.',
