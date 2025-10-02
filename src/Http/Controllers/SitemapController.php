663b9998e866f59e3677b57ad08ae2f51bdef502
Erin Dalzell <erin@silentz.co>
2025-10-02 03:57:30 -0700

    Remove external domains from sitemap (#399)
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>

--- src/Http/Controllers/SitemapController.php
+++ src/Http/Controllers/SitemapController.php
@@ -5,7 +5,10 @@ namespace Statamic\SeoPro\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+use Statamic\Facades;
 use Statamic\SeoPro\Sitemap\Sitemap;
+use Statamic\Sites\Site;
 
 class SitemapController extends Controller
 {
@@ -13,20 +16,22 @@ class SitemapController extends Controller
     {
         abort_unless(config('statamic.seo-pro.sitemap.enabled'), 404);
 
+        $key = request()->getHttpHost();
         $cacheUntil = Carbon::now()->addMinutes(config('statamic.seo-pro.sitemap.expire'));
+        $sites = Facades\Site::all()->filter(fn (Site $site) => Str::of($site->absoluteUrl())->startsWith(request()->schemeAndHttpHost()));
 
         if (config('statamic.seo-pro.sitemap.pagination.enabled', false)) {
-            $content = Cache::remember(Sitemap::CACHE_KEY.'_index', $cacheUntil, function () {
+            $content = Cache::remember(Sitemap::CACHE_KEY.'_'.$key.'_index', $cacheUntil, function () use ($sites) {
                 return view('seo-pro::sitemap_index', [
                     'xml_header' => '<?xml version="1.0" encoding="UTF-8"?>',
-                    'sitemaps' => app(Sitemap::class)->paginatedSitemaps(),
+                    'sitemaps' => app(Sitemap::class)->forSites($sites)->paginatedSitemaps(),
                 ])->render();
             });
         } else {
-            $content = Cache::remember(Sitemap::CACHE_KEY, $cacheUntil, function () {
+            $content = Cache::remember(Sitemap::CACHE_KEY.'_'.$key, $cacheUntil, function () use ($sites) {
                 return view('seo-pro::sitemap', [
                     'xml_header' => '<?xml version="1.0" encoding="UTF-8"?>',
-                    'pages' => app(Sitemap::class)->pages(),
+                    'pages' => app(Sitemap::class)->forSites($sites)->pages(),
                 ])->render();
             });
         }
