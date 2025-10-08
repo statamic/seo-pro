bd42f41f1c2f8e09bb5bc43822330e77845c62b8
Duncan McClean <duncan@duncanmcclean.com>
2025-10-08 16:05:11 +0100

    Wire up Vite & Vue 3 stuffz

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -4,9 +4,11 @@ import StatusIcon from './components/reporting/StatusIcon.vue';
 import IndexScore from './components/reporting/IndexScore.vue';
 import Report from './components/reporting/Report.vue';
 
-Statamic.$components.register('seo_pro-fieldtype', SeoProFieldtype);
-Statamic.$components.register('seo_pro_source-fieldtype', SourceFieldtype);
+Statamic.booted(() => {
+    Statamic.$components.register('seo_pro-fieldtype', SeoProFieldtype);
+    Statamic.$components.register('seo_pro_source-fieldtype', SourceFieldtype);
 
-Statamic.$components.register('seo-pro-status-icon', StatusIcon);
-Statamic.$components.register('seo-pro-report', Report);
-Statamic.$components.register('seo-pro-index-score', IndexScore);
+    Statamic.$components.register('seo-pro-status-icon', StatusIcon);
+    Statamic.$components.register('seo-pro-report', Report);
+    Statamic.$components.register('seo-pro-index-score', IndexScore);
+});
