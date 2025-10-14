eb28418c6b61875fa9744dc0af466f8313021ff6
Duncan McClean <duncan@duncanmcclean.com>
2025-10-14 15:07:22 +0100

    wip

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -3,12 +3,14 @@
 namespace Statamic\SeoPro\Http\Controllers;
 
 use Inertia\Inertia;
+use Statamic\Facades\File;
 
 class IndexController
 {
     public function __invoke()
     {
         return Inertia::render('SEOPro/Index', [
+            'icon' => File::get(__DIR__.'/../../../resources/svg/nav-icon.svg'),
             'canViewReports' => auth()->user()->can('view seo reports'),
             'canEditSiteDefaults' => auth()->user()->can('edit seo site defaults'),
             'canEditSectionDefaults' => auth()->user()->can('edit seo section defaults'),
