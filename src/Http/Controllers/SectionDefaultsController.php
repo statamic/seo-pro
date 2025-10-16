072ada80f16fb2f9e1154c46065d58e0ea289ad6
Duncan McClean <duncan@duncanmcclean.com>
2025-10-16 12:43:30 +0100

    permissions

--- src/Http/Controllers/SectionDefaultsController.php
+++ src/Http/Controllers/SectionDefaultsController.php
@@ -5,11 +5,14 @@ namespace Statamic\SeoPro\Http\Controllers;
 use Inertia\Inertia;
 use Statamic\Facades\Collection;
 use Statamic\Facades\Taxonomy;
+use Statamic\Facades\User;
 
 class SectionDefaultsController
 {
     public function index()
     {
+        abort_unless(User::current()->can('edit seo section defaults'), 403);
+
         return Inertia::render('seo-pro::SectionDefaults/Index', [
             'collections' => Collection::all(),
             'taxonomies' => Taxonomy::all(),
