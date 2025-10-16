56cdb46881eac4ba82124d5daad8358de70291d1
Duncan McClean <duncan@duncanmcclean.com>
2025-10-16 16:17:18 +0100

    Add update script to move files around

---
+++ src/UpdateScripts/MigrateToAddonSettings.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Statamic\SeoPro\UpdateScripts;
+
+use Illuminate\Support\Facades\File;
+use Statamic\Facades\Addon;
+use Statamic\Facades\YAML;
+use Statamic\UpdateScripts\UpdateScript;
+
+class MigrateToAddonSettings extends UpdateScript
+{
+    public function shouldUpdate($newVersion, $oldVersion)
+    {
+        return $this->isUpdatingTo('7.0.0');
+    }
+
+    public function update()
+    {
+        if (! File::exists($path = base_path('content/seo.yaml'))) {
+            return $this;
+        }
+
+        Addon::get('statamic/seo-pro')
+            ->settings()
+            ->set(YAML::file($path)->parse())
+            ->save();
+
+        File::delete($path);
+    }
+}
