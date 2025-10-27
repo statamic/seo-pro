de9b13fa04990ce078920279b3e21e1d9abc6884
Jack McDade <jack@jackmcdade.com>
2025-10-27 16:20:01 -0400

    fix icon path

--- src/Http/Controllers/CP/IndexController.php
+++ src/Http/Controllers/CP/IndexController.php
@@ -10,7 +10,7 @@ class IndexController
     public function __invoke()
     {
         return Inertia::render('seo-pro::Index', [
-            'icon' => File::get(__DIR__.'/../../../resources/svg/nav-icon.svg'),
+            'icon' => File::get(__DIR__.'/../../../../resources/svg/nav-icon.svg'),
             'canViewReports' => auth()->user()->can('view seo reports'),
             'canEditSiteDefaults' => auth()->user()->can('edit seo site defaults'),
             'canEditSectionDefaults' => auth()->user()->can('edit seo section defaults'),
