190cf28343f76c849bf7db2670c2f0d65728c36a
Duncan McClean <duncan@duncanmcclean.com>
2025-10-28 17:38:16 +0000

    Use a fallback when no favicon can be found

--- resources/js/components/fieldtypes/PreviewsFieldtype.vue
+++ resources/js/components/fieldtypes/PreviewsFieldtype.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Fieldtype } from '@statamic/cms';
 import { Field, injectPublishContext } from '@statamic/cms/ui';
-import { computed } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import striptags from "striptags";
 
 const emit = defineEmits(Fieldtype.emits);
@@ -110,6 +110,28 @@ const googleUrlComponents = computed(() => {
 
 	return [origin, ...pathSegments];
 });
+
+const faviconSrc = ref(null);
+const faviconErrored = ref(false);
+
+// Check if Google can find a favicon for the domain. If it can't, lets use a fallback.
+const fetchFavicon = async () => {
+	const url = `https://www.google.com/s2/favicons?domain=${domain.value}&sz=64}`;
+
+	try {
+		const response = await fetch(url, { method: 'HEAD' });
+
+		if (response.ok) {
+			faviconSrc.value = url;
+		} else {
+			faviconErrored.value = true;
+		}
+	} catch (error) {
+		faviconErrored.value = true;
+	}
+}
+
+onMounted(() => fetchFavicon());
 </script>
 
 <template>
@@ -119,7 +141,12 @@ const googleUrlComponents = computed(() => {
 				<div class="min-w-0">
 					<a class="flex flex-row items-center mb-1.5" :href="url" target="_blank">
 						<div class="size-[28px] bg-[#f3f5f6] !border !border-[#d2d2d2] dark:!border-[#5c5f5e] rounded-[50%] mr-3">
-							<img class="size-full" :src="`https://www.google.com/s2/favicons?domain=${domain}&sz=64}`" />
+							<img v-if="faviconSrc" class="size-full" :src="faviconSrc" />
+							<div v-if="faviconErrored" class="p-1">
+								<svg focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"></path>
+								</svg>
+							</div>
 						</div>
 						<div>
 							<div class="text-[#202124] dark:text-[#dadce0] text-sm" v-text="domain" />
