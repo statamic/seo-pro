c7013400d12916d1eab82f682d6a203c68f4d09e
helloDanuk <hello@danielkaufmann.ch>
2026-05-04 19:30:22 +0200

    German translations

--- lang/de_CH/validation.php
+++ lang/de_CH/validation.php
@@ -3,5 +3,7 @@
 return [
 
     'json_ld_omit_script_tags' => 'Bitte lass das `<script>`-Tag weg. SEO Pro fügt es automatisch hinzu.',
+    'redirect_url' => 'Muss eine absolute URL oder ein Pfad sein, der mit `/` beginnt.',
+    'unique_redirect_url' => 'Eine Weiterleitung mit dieser Quell-URL existiert bereits.',
 
 ];
