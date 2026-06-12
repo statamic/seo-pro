a136f0cf89130ccdd0e62b2cb2784b8916801895
Duncan McClean <duncan@duncanmcclean.com>
2026-06-12 11:09:15 +0100

    invalidate collections and taxonomies when section defaults are updated

--- src/Listeners/InvalidateStaticCache.php
+++ src/Listeners/InvalidateStaticCache.php
@@ -4,20 +4,25 @@ namespace Statamic\SeoPro\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Statamic\Facades\URL;
+use Statamic\SeoPro\Events\CollectionDefaultsSaved;
 use Statamic\SeoPro\Events\SiteDefaultsSaved;
+use Statamic\SeoPro\Events\TaxonomyDefaultsSaved;
 use Statamic\StaticCaching\Cacher;
+use Statamic\StaticCaching\Invalidator;
 use Statamic\Support\Arr;
 
 class InvalidateStaticCache implements ShouldQueue
 {
     private string|array $rules;
 
-    public function __construct(private Cacher $cacher)
-    {
+    public function __construct(
+        private Cacher $cacher,
+        private Invalidator $invalidator
+    ) {
         $this->rules = config('statamic.static_caching.invalidation.rules', []);
     }
 
-    public function handle(SiteDefaultsSaved $event): void
+    public function handleSiteDefaultsSaved(SiteDefaultsSaved $event): void
     {
         if (! config('statamic.static_caching.strategy')) {
             return;
@@ -41,5 +46,37 @@ class InvalidateStaticCache implements ShouldQueue
             ...$absoluteUrls,
             ...$prefixedRelativeUrls,
         ]);
+    }
+
+    public function handleCollectionDefaultsSaved(CollectionDefaultsSaved $event): void
+    {
+        if (! config('statamic.static_caching.strategy')) {
+            return;
+        }
+
+        if ($this->rules === 'all') {
+            $this->cacher->flush();
+
+            return;
+        }
+
+        $this->invalidator->invalidate($event->collection);
+    }
+
+    public function handleTaxonomyDefaultsSaved(TaxonomyDefaultsSaved $event): void
+    {
+        if (! config('statamic.static_caching.strategy')) {
+            return;
+        }
+
+        if ($this->rules === 'all') {
+            $this->cacher->flush();
+
+            return;
+        }
+
+        $event->taxonomy->queryTerms()->get()->each(function ($term) {
+            $this->invalidator->invalidate($term);
+        });
     }
 }
